62c8aac202611b796d355aa2aceb45290d9d52c9
Reneeliew2002@yahoo.com <Reneeliew2002@yahoo.com>
2024-05-13 11:24:18 -0400

    css restaurant-app commit

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -7,7 +7,7 @@ import './Chef.css';
 const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
-      <img src={images.chef} alt="chef_image" />
+      <img src={images.chef} alt="chef_image" style={{width:'92%'}} />
     </div>
     <div className="app__wrapper_info">
       <SubHeading title="Chef's word" />
@@ -22,7 +22,7 @@ const Chef = () => (
       </div>
 
       <div className="app__chef-sign">
-        <p>Kevin Luo</p>
+        <p className="p__opensans">Kevin Luo</p>
         <p className="p__opensans">Chef & Founder</p>
         <img src={images.sign} alt="sign_image" />
       </div>
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -11,10 +11,10 @@ const Gallery = () => {
   const scroll = (direction) => {
     const { current } = scrollRef;
 
-    if (direction === 'left') {
-      current.scrollLeft -= 300;
+    if (direction === 'left') { 
+      current.scrollLeft -= 300;   
     } else {
-      current.scrollLeft += 300;
+      current.scrollLeft += 300; 
     }
   };
 
